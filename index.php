28161e03a67bdc01e8ea2e6c8bda870e23a6f10d
shatzibitten <alex.polorotov@gmail.com>
2011-09-09 20:26:27 +0600

    Edited index.php via GitHub

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 include "lib/clCLient.class.php";
 include "lib/clQueries.php";
 
+header ('Content-type: text/html; charset=utf-8');
+
 $client = new clClient();
 $query  = new clReferencesQuery(clReferencesQuery::TOWN_CONTEXT);
 
